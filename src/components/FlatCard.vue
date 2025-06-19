df1b5487b6670a2cd68623f48b1bca07b2f829e4
mr-yeraliyev <yeraliyev.daryn@gmail.com>
2025-06-20 00:00:09 +0500

    fix: remove unnecessary dependencies

--- src/components/FlatCard.vue
+++ src/components/FlatCard.vue
@@ -1,21 +1,21 @@
 <template>
-  <a-card :title="flat.address" style="width: 100%">
+  <a-card hoverable style="width: 100%">
     <template #cover>
       <img :src="flat.image" :alt="flat.address" />
     </template>
-    <p>Площадь: {{ flat.area }} м²</p>
-    <p>Комнат: {{ flat.rooms }}</p>
+    <a-card-meta :title="flat.address">
+      <template #description>
+        <p>Площадь: {{ flat.area }} м²</p>
+        <p>Комнат: {{ flat.rooms }}</p>
+      </template>
+    </a-card-meta>
   </a-card>
 </template>
 
 <script setup lang="ts">
+import type { Flat } from '@/model'
+
 defineProps<{
-  flat: {
-    id: number
-    area: number
-    rooms: number
-    address: string
-    image: string
-  }
+  flat: Flat
 }>()
 </script>
